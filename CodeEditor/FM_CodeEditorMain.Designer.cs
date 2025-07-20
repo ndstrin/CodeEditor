bc5d03f8dd1b52f680062d9f77fe6e22f7fc32f6
Nate <Nate@LAPTOP-DKJ6VVT4>
2025-07-20 15:39:22 -0700

    Add project files.

---
+++ CodeEditor/FM_CodeEditorMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace CodeEditor
+{
+    partial class FM_CodeEditorMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip_Main = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip_Main = new System.Windows.Forms.StatusStrip();
+            this.openFileDialog_Editor = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog_Editor = new System.Windows.Forms.SaveFileDialog();
+            this.RTB_MainEditor = new System.Windows.Forms.RichTextBox();
+            this.BTN_Search = new System.Windows.Forms.Button();
+            this.TXB_SearchBox = new System.Windows.Forms.TextBox();
+            this.toolStripStatusLabel_TextStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip_Main.SuspendLayout();
+            this.statusStrip_Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip_Main
+            // 
+            this.menuStrip_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip_Main.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip_Main.Name = "menuStrip_Main";
+            this.menuStrip_Main.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip_Main.Size = new System.Drawing.Size(1140, 24);
+            this.menuStrip_Main.TabIndex = 0;
+            this.menuStrip_Main.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // statusStrip_Main
+            // 
+            this.statusStrip_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_TextStatus});
+            this.statusStrip_Main.Location = new System.Drawing.Point(0, 679);
+            this.statusStrip_Main.Name = "statusStrip_Main";
+            this.statusStrip_Main.Size = new System.Drawing.Size(1140, 22);
+            this.statusStrip_Main.TabIndex = 1;
+            this.statusStrip_Main.Text = "statusStrip1";
+            // 
+            // openFileDialog_Editor
+            // 
+            this.openFileDialog_Editor.FileName = "openFileDialog1";
+            // 
+            // RTB_MainEditor
+            // 
+            this.RTB_MainEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RTB_MainEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTB_MainEditor.Location = new System.Drawing.Point(0, 63);
+            this.RTB_MainEditor.Name = "RTB_MainEditor";
+            this.RTB_MainEditor.Size = new System.Drawing.Size(1140, 613);
+            this.RTB_MainEditor.TabIndex = 2;
+            this.RTB_MainEditor.Text = "";
+            this.RTB_MainEditor.TextChanged += new System.EventHandler(this.RTB_MainEditor_TextChanged);
+            // 
+            // BTN_Search
+            // 
+            this.BTN_Search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BTN_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Search.Location = new System.Drawing.Point(961, 34);
+            this.BTN_Search.Name = "BTN_Search";
+            this.BTN_Search.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Search.TabIndex = 3;
+            this.BTN_Search.Text = "Search";
+            this.BTN_Search.UseVisualStyleBackColor = true;
+            this.BTN_Search.Click += new System.EventHandler(this.BTN_Search_Click);
+            // 
+            // TXB_SearchBox
+            // 
+            this.TXB_SearchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TXB_SearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXB_SearchBox.Location = new System.Drawing.Point(767, 34);
+            this.TXB_SearchBox.Name = "TXB_SearchBox";
+            this.TXB_SearchBox.Size = new System.Drawing.Size(188, 23);
+            this.TXB_SearchBox.TabIndex = 4;
+            this.TXB_SearchBox.TextChanged += new System.EventHandler(this.TXB_SearchBox_TextChanged);
+            // 
+            // toolStripStatusLabel_TextStatus
+            // 
+            this.toolStripStatusLabel_TextStatus.Name = "toolStripStatusLabel_TextStatus";
+            this.toolStripStatusLabel_TextStatus.Size = new System.Drawing.Size(1125, 17);
+            this.toolStripStatusLabel_TextStatus.Spring = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(622, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search for Matching:";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // FM_CodeEditorMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 701);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXB_SearchBox);
+            this.Controls.Add(this.BTN_Search);
+            this.Controls.Add(this.RTB_MainEditor);
+            this.Controls.Add(this.statusStrip_Main);
+            this.Controls.Add(this.menuStrip_Main);
+            this.MainMenuStrip = this.menuStrip_Main;
+            this.Name = "FM_CodeEditorMain";
+            this.Text = "Custom Code Editor";
+            this.menuStrip_Main.ResumeLayout(false);
+            this.menuStrip_Main.PerformLayout();
+            this.statusStrip_Main.ResumeLayout(false);
+            this.statusStrip_Main.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip_Main;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip_Main;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_Editor;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog_Editor;
+        private System.Windows.Forms.RichTextBox RTB_MainEditor;
+        private System.Windows.Forms.Button BTN_Search;
+        private System.Windows.Forms.TextBox TXB_SearchBox;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_TextStatus;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        internal System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CodeEditor/FM_CodeEditorMain.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace CodeEditor
+{
+    public partial class FM_CodeEditorMain : Form
+    {
+        public string FileTextRaw = string.Empty;
+        public string CurrentFilePath = string.Empty;
+        public string CurrentFileName = string.Empty;
+        public string CurrentFileExt = string.Empty;
+
+        public FM_CodeEditorMain()
+        {
+            InitializeComponent();
+        }
+
+
+        //When the open file option is selected in the editor
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                //Will Open the file Dialog to let the user select thier own file to load into the editor.
+                openFileDialog_Editor.Title = "Open File to Edit";
+                DialogResult result =  openFileDialog_Editor.ShowDialog();
+                //If A File is selected then Process the file to load into the application
+                if (result == DialogResult.OK) 
+                {
+                    //Confirms if the file exist and has access to the file.
+                    if (File.Exists(openFileDialog_Editor.FileName))
+                    {
+                        //Load the text into the RawText value
+                        FileTextRaw = File.ReadAllText(openFileDialog_Editor.FileName).ToString();
+                        CurrentFilePath = openFileDialog_Editor.FileName;
+                        CurrentFileName = openFileDialog_Editor.SafeFileName;         
+                        //Load the Raw text into the RTB in the Editor
+                        RTB_MainEditor.Text = FileTextRaw;
+                    }
+                }
+            }
+            catch { MessageBox.Show("Error Opening File"); }
+        }
+        //Save to Current file or open the save as window if new file
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                if (CurrentFilePath != string.Empty)
+                {
+                    File.WriteAllText(CurrentFilePath, FileTextRaw);
+                }
+                else 
+                {
+                    SaveFileAS();
+                }
+            }
+            catch { MessageBox.Show("Error Saving File"); }
+        }
+
+        //Updates When Text is changed in the Editor
+        private void RTB_MainEditor_TextChanged(object sender, EventArgs e)
+        {
+            try 
+            { 
+                //Updates the Raw Text Value when updated.
+                FileTextRaw = RTB_MainEditor.Text; 
+            }
+            catch { MessageBox.Show("Error Updating Text"); }
+        }
+
+        //Function for Save As to File
+        private void SaveFileAS() 
+        {
+            try 
+            {
+                saveFileDialog_Editor.Title = "Save File As";
+                if (CurrentFilePath != String.Empty) 
+                { 
+                    saveFileDialog_Editor.FileName = CurrentFileName; 
+                }
+                DialogResult result = saveFileDialog_Editor.ShowDialog();
+                if (result == DialogResult.OK) 
+                {
+                    File.WriteAllText(saveFileDialog_Editor.FileName, FileTextRaw);
+                }
+            }
+            catch { MessageBox.Show("Error Saving As"); }
+        }
+
+
+
+        //Opens the Save As Fuction when Selected.
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileAS();
+        }
+
+
+        private void Search(string Value) 
+        {
+            try
+            {
+                int start = 0;
+                //Check for Empty Search Data
+                if (Value != string.Empty)
+                {
+                    //Checks if Search Data is in the Text Editor
+                    if (RTB_MainEditor.Text.Contains(Value))
+                    {
+                        //loops though the Text 
+                        while (start < RTB_MainEditor.TextLength)
+                        {
+                            toolStripStatusLabel_TextStatus.Text = $"Searching for: {Value}";
+                            //looks for the value in the document from start point and returns the location start value
+                            int wordstart = RTB_MainEditor.Find(Value, start, RichTextBoxFinds.None);
+                            if (wordstart != 1 && wordstart != -1)
+                            {
+                                RTB_MainEditor.SelectionStart = wordstart;
+                                RTB_MainEditor.SelectionLength = Value.Length;
+                                RTB_MainEditor.SelectionBackColor = Color.Yellow;
+                            }
+                            else
+                            {
+                                //Breaks the loop if no value is found
+                                break;
+                            }
+                            //Sets the Start value after last found Word
+                            start += wordstart + Value.Length;
+
+                        }
+
+
+
+                    }
+                }
+            }
+            catch(Exception ex) { MessageBox.Show("Error Searching" + ex); }
+            //Cleans up if it fails or not
+            finally 
+            {
+                toolStripStatusLabel_TextStatus.Text = "";
+            }
+
+        }
+
+        //Fuction to Clear Search Data Backcolor
+        private void ClearSearch() 
+        {
+            RTB_MainEditor.SelectionStart = 0;
+            RTB_MainEditor.SelectAll();
+            RTB_MainEditor.SelectionBackColor = Color.White;
+        }
+        //Starts the Search Proccess
+        private void BTN_Search_Click(object sender, EventArgs e)
+        {
+            Search(TXB_SearchBox.Text);
+        }
+
+        //Clears the Highlighed Text in the document if changed
+        private void TXB_SearchBox_TextChanged(object sender, EventArgs e)
+        {
+            ClearSearch();
+        }
+
+    }
+}
